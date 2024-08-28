7483b464778966daa2b10ddddd33ecbc152cca21
adujoseph <iamjosephadu@gmail.com>
2024-08-28 20:04:35 +0100

    modify plans module

--- src/plans/plans.entity.ts
+++ src/plans/plans.entity.ts
@@ -19,7 +19,7 @@ export class PlansEntity {
     sub_code: Number;
 
     @Column()
-    description: string;
+    subtitle: string;
 
     @Column()
     maxSubscriber: Number;
@@ -30,8 +30,11 @@ export class PlansEntity {
     @Column()
     pricing: Number;
 
+    @Column("text", { array: true })
+    features: string[];
+
     @Column()
-    features: string;
+    link: string;
 
     @CreateDateColumn()
     created_at: Date;
