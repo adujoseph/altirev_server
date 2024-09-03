6fa2e57b2cedb656419d9cb363138a868a20d843
ZAKARIYA RAJI <zakariyyaraji@gmail.com>
2024-09-03 19:19:43 +0100

    corrected aws issues

--- src/reports/s3.service.ts
+++ src/reports/s3.service.ts
@@ -14,10 +14,10 @@ export class S3Service {
 
     constructor() {
         this.s3 = new S3Client({
-            region: process.env.AWS_S3_REGION,
+            region: process.env.AWS_S3_REGION ?? '',
             credentials: {
-                accessKeyId: process.env.ACCESS_KEY_ID,
-                secretAccessKey: process.env.SECRET_ACCESS_KEY,
+                accessKeyId: process.env.ACCESS_KEY_ID ?? '',
+                secretAccessKey: process.env.SECRET_ACCESS_KEY ?? '',
             },
         });
     }
