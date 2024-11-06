1d8171317fb2af3b6584b1bc02d07f7e359e7c75
Zakariya Raji <zakariyyaraji@gmail.com>
2024-11-06 09:50:39 +0100

    update

---
+++ src/tenants/entities/tenant.entity.ts
@@ -0,0 +1,34 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { EntityRelationalHelper } from '../../utils/relational-entity-helper';
+import { UserEntity } from '../../users/persistence/entities/user.entity';
+
+@Entity({ name: 'tenants' })
+export class Tenant extends EntityRelationalHelper {
+    @PrimaryGeneratedColumn('uuid')
+    private id: string;
+
+    @Column()
+    private name: string;
+
+    @Column()
+    private moderator: string;
+
+    @Column()
+    private userCount: number;
+
+    @OneToOne(() => UserEntity, (user) => user.id)
+    createdBy: UserEntity;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
