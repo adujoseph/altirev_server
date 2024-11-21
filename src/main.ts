286b93008aad0a5f39029ff1153da735fe77bb62
Zakariyya Raji <zakariyyaraji@gmail.com>
2024-11-21 13:35:42 +0100

    updates removing redoc temporarily

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
 import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
 import * as bodyParser from 'body-parser';
-import { setupRedoc } from './redoc.setup';
+// import { setupRedoc } from './redoc.setup';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: true,
@@ -55,16 +55,7 @@ async function configureSwagger(
     app: any,
     configService: ConfigService<AllConfigType>,
 ) {
-    // const options = new DocumentBuilder()
-    //     .setTitle('Altirev API')
-    //     .setDescription('API documentation for Altirev Server')
-    //     .setVersion('1.0')
-    //     .addBearerAuth()
-    //     .build();
-    // const document = SwaggerModule.createDocument(app, options);
-    // SwaggerModule.setup('docs', app, document);
-
-    if (process.env.NODE_ENV === 'development') {
+    // if (process.env.NODE_ENV === 'development') {
         const options = new DocumentBuilder()
             .setTitle('Altirev ... counting that counts, v1')
             .setDescription(
@@ -75,9 +66,9 @@ async function configureSwagger(
             .build();
         const document = SwaggerModule.createDocument(app, options);
         SwaggerModule.setup('docs', app, document);
-    } else {
-        await setupRedoc(app);
-    }
+    // } else {
+        // await setupRedoc(app);
+    // }
 }
 
 function getPort(configService: ConfigService<AllConfigType>): number {
@@ -85,44 +76,3 @@ function getPort(configService: ConfigService<AllConfigType>): number {
 }
 
 void bootstrap();
-
-// async function bootstrap() {
-//     const app = await NestFactory.create(AppModule, { cors: true });
-//     useContainer(app.select(AppModule), { fallbackOnErrors: true });
-//     const configService = app.get(ConfigService<AllConfigType>);
-//     app.enableCors();
-//     app.enableShutdownHooks();
-//     app.setGlobalPrefix(
-//         configService.getOrThrow('app.apiPrefix', { infer: true }),
-//         {
-//             exclude: ['/'],
-//         },
-//     );
-//     app.enableVersioning({
-//         type: VersioningType.URI,
-//     });
-//     // Increase the request body size limit
-//     app.use(bodyParser.json({ limit: '50mb' })); // Adjust the size as needed
-//     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-//     app.useGlobalPipes(new ValidationPipe(validationOptions));
-//
-//     app.useGlobalInterceptors(
-//         // ResolvePromisesInterceptor is used to resolve promises in responses because class-transformer can't do it
-//         // https://github.com/typestack/class-transformer/issues/549
-//         new ResolvePromisesInterceptor(),
-//         new ClassSerializerInterceptor(app.get(Reflector)),
-//     );
-//
-//     const options = new DocumentBuilder()
-//         .setTitle('Altirev API')
-//         .setDescription('API documentation for Altirev Server')
-//         .setVersion('1.0')
-//         .addBearerAuth()
-//         .build();
-//
-//     const document = SwaggerModule.createDocument(app, options);
-//     SwaggerModule.setup('docs', app, document);
-//
-//     await app.listen(configService.getOrThrow('app.port', { infer: true }));
-// }
-// void bootstrap();
